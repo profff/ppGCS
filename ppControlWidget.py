3d856e7cd0d040d35091439c63ab52c4b2d79369
profff <ogaste@gmail.com>
2019-06-13 00:48:33 +0200

    add bind of axis and switch and save it in a json file neet to add reverse check boxes

--- ppControlWidget.py
+++ ppControlWidget.py
@@ -8,32 +8,62 @@ from PySide2.QtGui import QPainter,QIcon
 import time
 from ppDevice import *
 import pygame
-joytofunc={'axistostick':{'lv':1,
-                          'lh':0,
-                          'rv':3,
-                          'rh':2},
-           'axisreverse':{'lv':-1,
-                          'lh':1,
-                          'rv':-1,
-                          'rh':1},
-           'sticktofunc':{'pitch':'rv',
-                          'roll':'rh',
-                          'gaz':'lv',
-                          'yaw':'lh'},
-           'switchtofunc':{0:'start',
-                           2:'rth'}
-           }
+import json
+
+try:
+    with open('control_config.txt', 'r') as outfile:  
+        joytofunc=json.load(outfile)
+        print ("control config file loaded")
+except:
+    print ("default control config loaded")
+    joytofunc={'funcreverse':{'gaz':-1,
+                              'yaw':1,
+                              'pitch':-1,
+                              'roll':1},
+               'functoaxis':{'pitch':3,
+                             'roll':2,
+                             'gaz':1,
+                             'yaw':0},
+               'functoswitch':{'start':0,
+                               'rth':2}}
+joytofunc['switchtofunc']={y:x for x,y in joytofunc['functoswitch'].items()}
+bindaxis=None
+
+def bindtopitch():
+    global bindaxis
+    bindaxis='pitch'
+def bindtoroll():
+    global bindaxis
+    bindaxis='roll'
+def bindtoyaw():
+    global bindaxis
+    bindaxis='yaw'
+def bindtogaz():
+    global bindaxis
+    bindaxis='gaz'
+def bindtostart():
+    global bindaxis
+    bindaxis='start'
+def bindtorth():
+    global bindaxis
+    bindaxis='rth'
+def bindsave():
+    with open('control_config.txt', 'w') as outfile:  
+        json.dump(joytofunc, outfile)
+        outfile.close()
 
 class ppConfigControlWindow(QDialog):
     def __init__(self, parent=None):
         super(ppConfigControlWindow, self).__init__(parent)
         # Create widgets
+        s=self.parent().Sticks
         self.btPitch = QPushButton("pitch: 0")
         self.btRoll = QPushButton("roll: 0")
         self.btYaw = QPushButton("Yaw: 0")
         self.btGaz = QPushButton("Thrust: 0")
         self.btTakeoff = QPushButton("Takeoff/Landing: 0")
         self.btRth = QPushButton("RTH: 0")
+        self.btSave = QPushButton("Save")
         # Create layout and add widgets
         layout = QVBoxLayout()
         layout.addWidget(self.btPitch)
@@ -42,10 +72,25 @@ class ppConfigControlWindow(QDialog):
         layout.addWidget(self.btGaz)
         layout.addWidget(self.btTakeoff)
         layout.addWidget(self.btRth)
+        layout.addWidget(self.btSave)
         # Set dialog layout
         self.setLayout(layout)
-        # Add button signal to greetings slot
+        # Add button signal to slot
+        self.btPitch.clicked.connect(bindtopitch)
+        self.btRoll.clicked.connect(bindtoroll)
+        self.btYaw.clicked.connect(bindtoyaw)
+        self.btGaz.clicked.connect(bindtogaz)
+        self.btTakeoff.clicked.connect(bindtostart)
+        self.btRth.clicked.connect(bindtorth)
+        self.btSave.clicked.connect(bindsave)
 
+    def freshValues(self,p,r,y,g,to,rt):
+        self.btPitch.setText("pitch: %d"%p)
+        self.btRoll.setText("roll: %d"%r)
+        self.btYaw.setText("Yaw: %d"%y)
+        self.btGaz.setText("Thrust: %d"%g)
+        self.btTakeoff.setText("Takeoff/Landing: %d"%to)
+        self.btRth.setText("RTH: %d"%rt)
 
 
 class ppControlWidget(QWidget, QThread):
@@ -82,19 +127,22 @@ class ppControlWidget(QWidget, QThread):
             self._joystick.init()
             print(self._joystick.get_name())
             pygame.event.pump()
-            self._Sticks={'lv':self._joystick.get_axis(joytofunc['axistostick']['lv'])*joytofunc['axisreverse']['lv'],
-                          'lh':self._joystick.get_axis(joytofunc['axistostick']['lh'])*joytofunc['axisreverse']['lh'],
-                          'rv':self._joystick.get_axis(joytofunc['axistostick']['rv'])*joytofunc['axisreverse']['rv'],
-                          'rh':self._joystick.get_axis(joytofunc['axistostick']['rh'])*joytofunc['axisreverse']['rh']}
+            self._Sticks={'gaz':self._joystick.get_axis(joytofunc['functoaxis']['gaz'])*joytofunc['funcreverse']['gaz'],
+                          'yaw':self._joystick.get_axis(joytofunc['functoaxis']['yaw'])*joytofunc['funcreverse']['yaw'],
+                          'pitch':self._joystick.get_axis(joytofunc['functoaxis']['pitch'])*joytofunc['funcreverse']['pitch'],
+                          'roll':self._joystick.get_axis(joytofunc['functoaxis']['roll'])*joytofunc['funcreverse']['roll']}
+            self._Switchs={'start':self._joystick.get_button(joytofunc['functoswitch']['start']),
+                           'rth':self._joystick.get_button(joytofunc['functoswitch']['rth'])}
         else:
             self._joystick = None
-            self._Sticks={'lv':0,
-                          'lh':0,
-                          'rv':0,
-                          'rh':0}
-
+            self._Sticks={'gaz':0,
+                          'yaw':0,
+                          'pitch':0,
+                          'roll':0}
+            self._Switchs={'start':0,
+                           'rth':0}
         self.btconfig.clicked.connect(self.config)
-        
+        self.ConfigDialog=ppConfigControlWindow(parent=self)
         self.start()
         
     def __del__(self):
@@ -112,9 +160,8 @@ class ppControlWidget(QWidget, QThread):
         rc=(r.height()*0.1)/2
         painter.drawEllipse(c1,r1,r1)
         painter.drawEllipse(c2,r1,r1)
-        global axistostick
-        c1=QPoint(r.width()/4+(self._Sticks['lh']*r1),r.height()/2-(self._Sticks['lv']*r1))
-        c2=QPoint(3*r.width()/4+(self._Sticks['rh']*r1),r.height()/2-(self._Sticks['rv']*r1))
+        c1=QPoint(r.width()/4+(self._Sticks['yaw']*r1),r.height()/2-(self._Sticks['gaz']*r1))
+        c2=QPoint(3*r.width()/4+(self._Sticks['roll']*r1),r.height()/2-(self._Sticks['pitch']*r1))
         painter.drawEllipse(c1,rc,rc)
         painter.drawEllipse(c2,rc,rc)
 
@@ -122,41 +169,67 @@ class ppControlWidget(QWidget, QThread):
         pass
     
     def run(self):
+        global bindaxis
         while(self._running):
             pygame.event.pump()
             if(self._joycnt>0):
-                self._Sticks={'lv':self._joystick.get_axis(joytofunc['axistostick']['lv'])*joytofunc['axisreverse']['lv'],
-                              'lh':self._joystick.get_axis(joytofunc['axistostick']['lh'])*joytofunc['axisreverse']['lh'],
-                              'rv':self._joystick.get_axis(joytofunc['axistostick']['rv'])*joytofunc['axisreverse']['rv'],
-                              'rh':self._joystick.get_axis(joytofunc['axistostick']['rh'])*joytofunc['axisreverse']['rh']}
+                self._Sticks={'gaz':self._joystick.get_axis(joytofunc['functoaxis']['gaz'])*joytofunc['funcreverse']['gaz'],
+                              'yaw':self._joystick.get_axis(joytofunc['functoaxis']['yaw'])*joytofunc['funcreverse']['yaw'],
+                              'pitch':self._joystick.get_axis(joytofunc['functoaxis']['pitch'])*joytofunc['funcreverse']['pitch'],
+                              'roll':self._joystick.get_axis(joytofunc['functoaxis']['roll'])*joytofunc['funcreverse']['roll']}
+                self._Switchs={'start':self._joystick.get_button(joytofunc['functoswitch']['start']),
+                               'rth':self._joystick.get_button(joytofunc['functoswitch']['rth'])}
+                
+            p=round(100*self._Sticks['pitch'])
+            r=round(100*self._Sticks['roll'])
+            y=round(100*self._Sticks['yaw'])
+            t=round(100*self._Sticks['gaz'])
+            to=self._Switchs['start']
+            rt=self._Switchs['rth']
+                           
+            self.ConfigDialog.freshValues(p,r,y,t,to,rt)
 
             if(self._instance is not None):
-                p=round(100*self._Sticks[joytofunc['sticktofunc']['pitch']])
-                r=round(100*self._Sticks[joytofunc['sticktofunc']['roll']])
-                y=round(100*self._Sticks[joytofunc['sticktofunc']['yaw']])
-                t=round(100*self._Sticks[joytofunc['sticktofunc']['gaz']])
                 self._instance.PCMD(p,r,y,t)
 
-                for event in pygame.event.get(): # User did something
+                    
+            for event in pygame.event.get(): # User did something
+                if not (bindaxis == None):
+                    if event.type == pygame.JOYAXISMOTION:
+                        if abs(event.value)>0.5:
+                            joytofunc['functoaxis'][bindaxis]=event.axis
+                            bindaxis=None
                     if event.type == pygame.JOYBUTTONDOWN:
-                        if(event.button in joytofunc['switchtofunc'].keys()):
-                            if(joytofunc['switchtofunc'][event.button]=='start'):
-                                try: 
-                                    self._instance.tkof_lnd_emcy()
-                                except:
-                                    print("no device selected")
-                            if(joytofunc['switchtofunc'][event.button]=='rth'):
-                                try: 
-                                    self._instance.rth()
-                                except:
-                                    print("no device selected")
+                        joytofunc['functoswitch'][bindaxis]=event.button
+                        bindaxis=None
+
+                        
+
+                
+                if event.type == pygame.JOYBUTTONDOWN:
+                    if(event.button in joytofunc['switchtofunc'].keys()):
+                        if(joytofunc['switchtofunc'][event.button]=='start'):
+                            try: 
+                                self._instance.tkof_lnd_emcy()
+                            except:
+                                print("no device selected")
+                        if(joytofunc['switchtofunc'][event.button]=='rth'):
+                            try: 
+                                self._instance.rth()
+                            except:
+                                print("no device selected")
 
             self.update()
             time.sleep(self._periodicity)
 
     def config(self):
-        w=ppConfigControlWindow(parent=self)
+        w=self.ConfigDialog
         w.show()
+
+    @property
+    def Sticks(self):
+        return self._Sticks
+    
     @property
     def device(self):
         return self._instance
